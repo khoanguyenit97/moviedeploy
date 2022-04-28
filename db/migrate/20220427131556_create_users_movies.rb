412f6dc2c5f62c7e89a16a3b49261274e0085015
khoanda10 <khoanda10@gmail.com>
2022-04-28 20:15:32 +0700

    six

--- db/migrate/20220427131556_create_users_movies.rb
+++ db/migrate/20220427131556_create_users_movies.rb
@@ -1,6 +1,6 @@
 class CreateUsersMovies < ActiveRecord::Migration[6.1]
   def change
-    create_table :users_movies do |t|
+    create_table :user_movies do |t|
       t.integer :user_id
       t.integer :movie_id
       t.timestamps
